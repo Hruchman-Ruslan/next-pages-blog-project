2ced140359e4372076f4a42a9e24ab02415c2c8b
Ruslan <Giovingo92@gmail.com>
2024-07-12 19:18:47 +0300

    feat: posts component

---
+++ src/components/posts/post-item.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import classes from "./post-item.module.css";
+
+export interface PostItemProps {
+  title: string;
+  image: string;
+  excerpt: string;
+  date: string;
+  slug: string;
+}
+
+export default function PostItem({
+  title,
+  image,
+  excerpt,
+  date,
+  slug,
+}: PostItemProps) {
+  const formattedDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  const imagePath = `/images/posts/${slug}/${image}`;
+
+  return (
+    <li className={classes.post}>
+      <Link href={slug}>
+        <div className={classes.image}>
+          <Image src={imagePath} alt={title} width={300} height={200} />
+        </div>
+        <div className={classes.content}>
+          <h3>{title}</h3>
+          <time>{formattedDate}</time>
+          <p>{excerpt}</p>
+        </div>
+      </Link>
+    </li>
+  );
+}
