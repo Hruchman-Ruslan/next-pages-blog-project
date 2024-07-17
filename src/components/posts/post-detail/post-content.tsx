765567f0098db4b7f174a88c46a9bce93f7aabdf
Ruslan <Giovingo92@gmail.com>
2024-07-17 21:26:25 +0300

    feat: rendering code snippets from markdown

--- src/components/posts/post-detail/post-content.tsx
+++ src/components/posts/post-detail/post-content.tsx
@@ -1,11 +1,19 @@
 import ReactMarkdown from "react-markdown";
 import Image from "next/image";
 
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+
 import { IPost } from "@/types/post";
 
 import PostHeader from "./post-header";
 
 import classes from "./post-content.module.css";
+
+interface CodeProps {
+  className: string;
+  children: string;
+}
 
 export interface PostContentProps {
   post: IPost;
@@ -33,6 +41,15 @@ export default function PostContent({ post }: PostContentProps) {
         );
       }
       return <p>{paragraph.children}</p>;
+    },
+    code({ className, children }: CodeProps) {
+      const language = className.split("-")[1];
+
+      return (
+        <SyntaxHighlighter style={atomDark} language={language}>
+          {children}
+        </SyntaxHighlighter>
+      );
     },
   };
 
